3ee07babdc3ab8f6a9fc2fb4bd748eeb7a5c595a
Hesam Fattahi <88786350+hesam-fattahi@users.noreply.github.com>
2023-05-10 20:12:13 +0330

    added password visibility toggle

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,24 +1,45 @@
 import "./Input.scss";
+import { useState } from "react";
 
-const Input = ({ type, label, icon, placeholder, value, onChange, error }) => {
+const Input = (props) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+
   return (
-    <div className={`input ${error && "error"}`}>
-      <label className="input__label">{label}</label>
+    <div className={`input ${props.error && "error"}`}>
+      <label className="input__label">{props.label}</label>
       <div className="input__field-wrapper">
         <ion-icon
-          name={`${error ? "alert-circle" : icon}-outline`}
+          name={`${props.error ? "alert-circle" : props.icon}-outline`}
           class="input__icon"
         ></ion-icon>
         <input
-          type={type}
+          type={props.type === "password" && showPassword ? "text" : props.type}
           className="input__field"
-          placeholder={placeholder}
-          onChange={onChange}
-          value={value}
+          placeholder={props.placeholder}
+          onChange={props.onChange}
+          onBlur={props.onBlur}
+          value={props.value}
         />
+        {props.type === "password" && (
+          <button
+            type="button"
+            className="input__toggle"
+            onClick={handleTogglePasswordVisibility}
+            aria-label={`${showPassword ? "Hide" : "Show"} password`}
+          >
+            <ion-icon
+              name={`${showPassword ? "eye-off" : "eye"}-outline`}
+              class="input__icon input__icon--toggle"
+            ></ion-icon>
+          </button>
+        )}
       </div>
 
-      {error && <p className="input__error">{error}</p>}
+      {props.error && <p className="input__error">{props.error}</p>}
     </div>
   );
 };
